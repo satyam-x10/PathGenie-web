41b8878c428554720506eaa8ef4a618b63b37810
satyam kumar <satyamx10@gmail.com>
2024-10-21 22:38:37 +0530

    new user create

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,33 @@
-import React from "react";
+'use client';
+import React, { useEffect } from "react";
 import HomePage from "../components/Homepage.js";
-const page = () => {
+import { useUser } from "@clerk/nextjs";
+import { createUser } from "@/utils/actions/userAction.js";
+
+const Page = () => {
+  const { isLoaded, isSignedIn, user } = useUser();
+
+  useEffect(() => {
+    const handleUserCreation = async () => {
+      if (isLoaded) {
+        if (isSignedIn) {
+          try {
+            await createUser({ email: user?.emailAddresses[0].emailAddress });
+
+            console.log(user); // Logs user details when they are loaded and signed in
+          } catch (error) {
+            console.error("Error creating user:", error);
+          }
+        } else {
+          console.log("User is not signed in.");
+        }
+      }
+    };
+
+    handleUserCreation();
+  }, [isLoaded, isSignedIn, user]);
+
   return <HomePage />;
 };
 
-export default page;
+export default Page;
